a642048f2f53eec4f26f0da6bc82c35968abd914
Anuj Pahuja <anuj.pahuja@tonboimaging.com>
2015-10-03 12:08:31 +0530

    Untrack node_modules directory

--- Client/node_modules/opencv/examples/make-example-files.sh
+++
@@ -1,1 +0,0 @@
-mencoder examples/motion.mov -ovc raw -vf format=i420 -nosound -o  examples/motion.avi
--- Client/node_modules/opencv/src/Constants.h
+++
@@ -1,6 +0,0 @@
-#include "OpenCV.h"
-
-class Constants: public node::ObjectWrap {
- public:
-    static void Init(Handle<Object> target);
-};
