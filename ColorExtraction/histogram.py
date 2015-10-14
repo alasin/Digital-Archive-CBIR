4eee78101a55c7539b3069882bc4fbe1c8696b65
Anuj Pahuja <anuj.pahuja@tonboimaging.com>
2015-10-14 15:21:43 +0530

    Recognise .JPG and .png image extensions

--- ColorExtraction/histogram.py
+++ ColorExtraction/histogram.py
@@ -13,11 +13,11 @@ class RGBHistogram:
 	def describe(self, image):
 		hist = cv2.calcHist([image], [0, 1, 2],
 			None, self.bins, [0, 256, 0, 256, 0, 256])
-		hist = cv2.normalize(hist)
+		cv2.normalize(hist, hist)
 		#print type(hist)
 		#print type(hist.flatten())
 		return hist.flatten()
-  
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required = True,
@@ -25,7 +25,7 @@ ap.add_argument("-d", "--dataset", required = True,
 ap.add_argument("-i", "--index", required = True,
 	help = "Path to where the computed index will be stored")
 args = vars(ap.parse_args())
- 
+
 big_index = []
 index = {}
 #small_index = []
@@ -35,7 +35,11 @@ desc = RGBHistogram([8, 8, 8])
 f = open(args["index"], "w")
 #f2 = open('dummyindex.xml', "w")
 
-for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
+imagePaths = glob.glob(args["dataset"] + "/*.jpg")
+imagePaths.extend(glob.glob(args["dataset"] + "/*.JPG"))
+imagePaths.extend(glob.glob(args["dataset"] + "/*.png"))
+
+for imagePath in imagePaths:
 	k = imagePath[imagePath.rfind("/") + 1:]
 	image = cv2.imread(imagePath)
 	features = desc.describe(image)
@@ -43,12 +47,12 @@ for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
 	index[k] = features.tolist()
 	#big_index.append(index)
 	#print type(index[k])
-		
+
 #print type(index)
 #xml = dicttoxml.dicttoxml(big_index, custom_root='images')
 xml = dicttoxml.dicttoxml(index, attr_type=False)
 f.write(xml)
-f.close()	
+f.close()
 
 #f = open(args["index"], "w")
 #f.write(cPickle.dumps(index))
