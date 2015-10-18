62df47ae362f3764d0d38157be341fa91cf0b6f5
Anuj Pahuja <anuj.pahuja@tonboimaging.com>
2015-10-18 17:53:53 +0530

    Refactor Server code

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,7 @@ var app = express()
 
 /// Include the express body parser
 app.configure(function () {
-  app.use(express.bodyParser());
+    app.use(express.bodyParser());
 });
 
 app.set('view engine', 'jade');
@@ -22,84 +22,82 @@ var xmlDoc, dummy, elem;
 // Post files
 app.post('/upload', function(req, res) {
 
-	fs.readFile(req.files.image.path, function (err, data) {
-		var imageName = req.files.image.name
+    fs.readFile(req.files.image.path, function (err, data) {
+        var imageName = req.files.image.name
 
-		if(!imageName) {
-			console.log("There was an error")
-			res.redirect("/");
-			res.end();
+        if(!imageName) {
+            console.log("There was an error")
+            res.redirect("/");
+            res.end();
 
-		} else {
-		  var newPath = "../Database/fullsize/" + imageName;
-		  var thumbPath = "../Database/thumbs/" + imageName;
+        } else {
+            var newPath = "../Database/fullsize/" + imageName;
+            var thumbPath = "../Database/thumbs/" + imageName;
 
-		  // Write file to Database/fullsize and Database/thumbs folder
-		  fs.writeFile(newPath, data, function (err) {
+            // Write file to Database/fullsize and Database/thumbs folder
+            fs.writeFile(newPath, data, function (err) {
 
-		  	var locationtag = req.body.locationtag;
-		  	var periodtag = req.body.periodtag;
-			var styletag = req.body.styletag;
-		  	var themetag = req.body.themetag;
-			var objecttag = req.body.objecttag;
-			var count = 0;
+                var locationtag = req.body.locationtag;
+                var periodtag = req.body.periodtag;
+                var styletag = req.body.styletag;
+                var themetag = req.body.themetag;
+                var objecttag = req.body.objecttag;
+                var count = 0;
 
-			fs.writeFileSync(thumbPath, im.convert({
-			    srcData: fs.readFileSync(newPath),
-			    width: 100,
-			    height: 100,
-			    resizeStyle: 'aspectfill', // is the default, or 'aspectfit' or 'fill'
-			    gravity: 'Center' // optional: position crop area when using 'aspectfill'
-			}));
+                fs.writeFileSync(thumbPath, im.convert({
+                    srcData: fs.readFileSync(newPath),
+                    width: 100,
+                    height: 100,
+                    resizeStyle: 'aspectfill', // is the default, or 'aspectfit' or 'fill'
+                    gravity: 'Center' // optional: position crop area when using 'aspectfill'
+                }));
 
+                fs.readFile('../Database/tags.xml', 'utf8', function (err, data) {
+                    console.log(thumbPath)
+                    if (err)
+                        throw err;
 
-			fs.readFile('../Database/tags.xml', 'utf8', function (err, data) {
+                    console.log(data);
+                    xmlDoc = libxmljs.parseXml(data);
+                    var rootnode = xmlDoc.root();
+                    elem = new libxmljs.Element(xmlDoc, 'image');
+                    dummy = rootnode.addChild(elem);
+                    dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'location', locationtag));
+                    dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'period', periodtag));
+                    dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'style', styletag));
+                    dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'theme', themetag));
+                    dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'object', objecttag));
 
-			  console.log(thumbPath)
-			  if (err)
-			    throw err;
+                    dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'source', imageName));
 
-			  console.log(data);
-			  xmlDoc = libxmljs.parseXml(data);
-			  var rootnode = xmlDoc.root();
-			  elem = new libxmljs.Element(xmlDoc, 'image');
-			  dummy = rootnode.addChild(elem);
-			  dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'location', locationtag));
-			  dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'period', periodtag));
-			  dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'style', styletag));
-			  dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'theme', themetag));
-			  dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'object', objecttag));
+                    console.log(xmlDoc.toString());
 
-			  dummy = elem.addChild(new libxmljs.Element(xmlDoc, 'source', imageName));
+                    fs.writeFile('../Database/tags.xml', xmlDoc.toString(), function (err) {
+                        if (err)
+                        throw err;
+                        console.log('Updated!');
+                    });
+                });
 
-			  console.log(xmlDoc.toString());
-
-			  fs.writeFile('../Database/tags.xml', xmlDoc.toString(), function (err) {
-
-			    if (err)
-			      throw err;
-			    console.log('Updated!');
-			  });
-			});
-			res.redirect("/uploads/" + imageName);
-		  });
-		}
-	});
+                res.redirect("/uploads/" + imageName);
+            });
+        }
+    });
 });
 
 /// Show files
 app.get('/uploads/:file', function (req, res) {
-	file = req.params.file;
-	var img = fs.readFileSync("../Database/fullsize/" + file);
-	res.writeHead(200, {'Content-Type': 'image/jpg'});
-	res.end(img, 'binary');
+    file = req.params.file;
+    var img = fs.readFileSync("../Database/fullsize/" + file);
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
+    res.end(img, 'binary');
 });
 
 app.get('/uploads/thumbs/:file', function (req, res) {
-	file = req.params.file;
-	var img = fs.readFileSync("../Database/thumbs/" + file);
-	res.writeHead(200, {'Content-Type': 'image/jpg'});
-	res.end(img, 'binary');
+    file = req.params.file;
+    var img = fs.readFileSync("../Database/thumbs/" + file);
+    res.writeHead(200, {'Content-Type': 'image/jpg'});
+    res.end(img, 'binary');
 });
 
 app.listen(8080)
--- Server/public/javascripts/script.js
+++ Server/public/javascripts/script.js
@@ -1,4 +1,3 @@
-
 function initAll() {
 	//console.log(document.getElementById("searchBox").value);
 	$.ajax({
@@ -14,23 +13,18 @@ function initAll() {
 			console.log("Image " + (i+1) + " Location: localhost:8080/uploads/" + data.result[i] + '\n');
 			var dummyId = "image" + (i+1);
 			var dummyLinkId = "link" + (i+1);
-			document.getElementById(dummyId).src= "http://localhost:8080/uploads/thumbs/" + data.result[i];
-			document.getElementById(dummyLinkId).href= "http://localhost:8080/uploads/" + data.result[i];
+			document.getElementById(dummyId).src = "http://localhost:8080/uploads/thumbs/" + data.result[i];
+			document.getElementById(dummyLinkId).href = "http://localhost:8080/uploads/" + data.result[i];
 		}
-
 	});
 }
 
 function imageSearch() {
-
 	$.ajax({
 		type: "POST",
 		url: "/searchImage",
 		data: { searchQuery: document.getElementById("imageFilename").src}
 	})
-
 	.done(function(data) {
-
-
 	});
 }
